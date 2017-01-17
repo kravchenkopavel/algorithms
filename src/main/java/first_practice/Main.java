54dbcc28311464bab6d0649d7dfb4517e97ca875
krava <krava>
2017-01-17 23:16:03 +0200

    first successfully empty build

---
+++ src/main/java/first_practice/Main.java
@@ -0,0 +1,14 @@
+package first_practice;
+
+import org.apache.log4j.Logger;
+/**
+ * Created by 1 on 17.01.2017.
+ */
+public class Main {
+
+    private static final Logger LOG = Logger.getLogger(Main.class);
+
+    public static void main (String[] args){
+
+    }
+}
